0d9405ee581542092f3dbc9a9acc5f248f7b1103
katahirado <yuichi@katahirado.jp>
2012-05-07 02:40:56 +0900

    add DM text count calculate

--- src/jp/katahirado/android/tsubunomi/SendDMActivity.java
+++ src/jp/katahirado/android/tsubunomi/SendDMActivity.java
@@ -5,18 +5,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.SpannableStringBuilder;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.twitter.Extractor;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -29,6 +34,7 @@ public class SendDMActivity extends Activity {
     private Twitter twitter;
     private SharedManager sharedManager;
     private TextView dmTweetCount;
+    private Extractor extractor;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +42,35 @@ public class SendDMActivity extends Activity {
 
         sharedManager = SharedManager.getInstance();
         sharedManager.sharedPreferencesInit(getSharedPreferences(Const.PREFERENCE_NAME, MODE_PRIVATE));
+        extractor = new Extractor();
 
-        dmText = (EditText) findViewById(R.id.dmText);
         dmTweetCount = (TextView) findViewById(R.id.dmTweetCount);
+        dmText = (EditText) findViewById(R.id.dmText);
         dmButton = (Button) findViewById(R.id.dmButton);
+
         dmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sendDM();
+            }
+        });
+
+        dmText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int diffCount = calculateShortURLsLength(editable.toString());
+                int tCount = Const.TWEET_COUNT_DEFAULT + diffCount - editable.length();
+                dmTweetCount.setText(String.valueOf(tCount));
+                InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                manager.hideSoftInputFromWindow(dmText.getWindowToken(), InputMethodManager.HIDE_IMPLICIT_ONLY);
             }
         });
         Intent intent;
@@ -66,6 +93,32 @@ public class SendDMActivity extends Activity {
                 setTitle(getString(R.string.app_name) + " : " + screenName + "にダイレクトメッセージ");
             }
         }
+    }
+
+    private int calculateShortURLsLength(String tText) {
+        List<String> urls = extractor.extractURLs(tText);
+        int diffCount = 0;
+        for (String url : urls) {
+            int shrinkLength;
+            int checkLength;
+            int urlLength = url.length();
+            if (url.indexOf("http://") == 0) {
+                checkLength = shrinkLength = sharedManager.getPrefInt(Const.SHORT_URL_LENGTH, 0);
+            } else if (url.indexOf("https://") == 0) {
+                checkLength = shrinkLength = sharedManager.getPrefInt(Const.SHORT_URL_LENGTH_HTTPS, 0);
+            } else {
+                //プロトコル無しurl
+                shrinkLength = sharedManager.getPrefInt(Const.SHORT_URL_LENGTH, 0);
+                checkLength = shrinkLength - 7;
+            }
+
+            if (urlLength >= checkLength) {
+                diffCount += urlLength - shrinkLength;
+            } else {
+                diffCount -= shrinkLength - urlLength;
+            }
+        }
+        return diffCount;
     }
 
     private void connectTwitter() {
