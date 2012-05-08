a4779ccbb46d098ae14a26ca48022b17e0711465
katahirado <yuichi@katahirado.jp>
2012-05-08 23:29:30 +0900

    change button eventListener implements View.OnClickListener

--- src/jp/katahirado/android/tsubunomi/UserTimelineActivity.java
+++ src/jp/katahirado/android/tsubunomi/UserTimelineActivity.java
@@ -7,15 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import twitter4j.Status;
+
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
  * Author: yuichi_katahira
  */
-public class UserTimelineActivity extends Activity {
+public class UserTimelineActivity extends Activity implements View.OnClickListener{
     private Button searchButton;
     private EditText screenNameText;
-    private ListView tweetList;
+    private ListView listView;
+    private ArrayList<Status> tweetList;
+    private TweetListAdapter tweetListAdapter;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +28,22 @@ public class UserTimelineActivity extends Activity {
 
         screenNameText = (EditText) findViewById(R.id.screen_name_text);
         searchButton = (Button) findViewById(R.id.search_button);
-        tweetList = (ListView) findViewById(R.id.tweet_list);
+        listView = (ListView) findViewById(R.id.tweet_list);
+        tweetList = new ArrayList<Status>();
+        tweetListAdapter = new TweetListAdapter(this,tweetList);
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.search_button:
                 SpannableStringBuilder builder = (SpannableStringBuilder) screenNameText.getText();
                 String query = builder.toString();
-            }
-        });
+
+                UserTimelineTask task = new UserTimelineTask(this,tweetListAdapter);
+                task.execute(query);
+                break;
+        }
     }
 }
