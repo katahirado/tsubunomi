23cbd3c6ea35e6df56ae8e2b1e514667575b2571
katahirado <yuichi@katahirado.jp>
2012-05-09 09:20:05 +0900

    modified getView method

--- src/jp/katahirado/android/tsubunomi/TweetListAdapter.java
+++ src/jp/katahirado/android/tsubunomi/TweetListAdapter.java
@@ -1,10 +1,13 @@
 package jp.katahirado.android.tsubunomi;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 import twitter4j.Status;
 
 import java.util.List;
@@ -16,6 +19,9 @@ import java.util.List;
 public class TweetListAdapter extends ArrayAdapter<Status>{
 
     private LayoutInflater layoutInflater;
+    private TextView screenName;
+    private TextView createdAt;
+    private TextView tweetText;
 
     public TweetListAdapter(Context context,  List<Status> objects) {
         super(context, 0, objects);
@@ -30,6 +36,15 @@ public class TweetListAdapter extends ArrayAdapter<Status>{
             view= layoutInflater.inflate(R.layout.tweet_row,null);
         }
 
+        Status status = this.getItem(position);
+        if(status!=null){
+            screenName = (TextView) view.findViewById(R.id.row_screen_name);
+            screenName.setText(status.getUser().getScreenName());
+            createdAt = (TextView) view.findViewById(R.id.row_created_at);
+            createdAt.setText(" " + status.getCreatedAt().toString());
+            tweetText = (TextView) view.findViewById(R.id.row_tweet_text);
+            tweetText.setText(status.getText());
+        }
         return view;
     }
 }
