ce4dadcb69c1b28db00f9aef47e5a2111968be66
katahirado <yuichi@katahirado.jp>
2012-05-26 23:51:34 +0900

    autocompleteの単語を削除するactivityを追加

---
+++ src/jp/katahirado/android/tsubunomi/activity/ScreenNamesManageActivity.java
@@ -0,0 +1,127 @@
+package jp.katahirado.android.tsubunomi.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.*;
+import jp.katahirado.android.tsubunomi.Const;
+import jp.katahirado.android.tsubunomi.R;
+import jp.katahirado.android.tsubunomi.SharedManager;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: yuichi_katahira
+ */
+public class ScreenNamesManageActivity extends Activity
+        implements View.OnClickListener, AdapterView.OnItemClickListener {
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> screenNames;
+    private SharedManager sharedManager;
+    private EditText screenNameText;
+    private String query;
+    private ListView manageList;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_names_manage);
+        setTitle(getString(R.string.app_name) + " : Delete autocomplete word");
+
+        sharedManager = new SharedManager(getSharedPreferences(Const.PREFERENCE_NAME, MODE_PRIVATE));
+        screenNames = sharedManager.getScreenNames();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, screenNames);
+
+        manageList = (ListView) findViewById(R.id.screen_name_manage_list);
+        Button searchButton = (Button) findViewById(R.id.screen_name_search_button);
+        screenNameText = (EditText) findViewById(R.id.manage_screen_name_text);
+
+        searchButton.setOnClickListener(this);
+        adapter.sort(new LowerCaseComparator());
+        manageList.setAdapter(adapter);
+        manageList.setOnItemClickListener(this);
+        manageList.requestFocus();
+        manageList.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                hideIME();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.screen_name_search_button:
+                SpannableStringBuilder builder = (SpannableStringBuilder) screenNameText.getText();
+                query = builder.toString();
+                if (query.length() == 0) {
+                    return;
+                }
+                screenNames = screenNamesFilter();
+                adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, screenNames);
+                adapter.sort(new LowerCaseComparator());
+                manageList.setAdapter(adapter);
+                hideIME();
+                screenNameText.setText("");
+                break;
+        }
+    }
+
+    private void hideIME() {
+        InputMethodManager manager =
+                (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        manager.hideSoftInputFromWindow(screenNameText.getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    private ArrayList<String> screenNamesFilter() {
+        if (query.equals("*")) {
+            return sharedManager.getScreenNames();
+        }
+        ArrayList<String> list = new ArrayList<String>();
+        for (String s : screenNames) {
+            if (s.toLowerCase().startsWith(query.toLowerCase())) {
+                list.add(s);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        final String screenName = screenNames.get(position);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(screenName + " を削除してもよろしいですか?").setCancelable(false);
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                adapter.remove(screenName);
+                screenNames.remove(screenName);
+                sharedManager.setScreenNames(screenNames);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    private class LowerCaseComparator implements Comparator<String> {
+        @Override
+        public int compare(String s, String s1) {
+            return s.compareToIgnoreCase(s1);
+        }
+    }
+}
