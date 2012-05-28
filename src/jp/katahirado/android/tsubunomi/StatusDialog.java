ed0c5cb292cf003a9fa0a098402605694c34426d
katahirado <yuichi@katahirado.jp>
2012-05-29 05:27:14 +0900

    ユーザータイムラインのツイートメニューダイアログを作成したスーパークラスを継承するように修正

--- src/jp/katahirado/android/tsubunomi/StatusDialog.java
+++ src/jp/katahirado/android/tsubunomi/StatusDialog.java
@@ -1,13 +1,9 @@
 package jp.katahirado.android.tsubunomi;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import jp.katahirado.android.tsubunomi.activity.TsubunomiActivity;
 import twitter4j.Status;
 
@@ -15,32 +11,12 @@ import twitter4j.Status;
  * Created with IntelliJ IDEA.
  * Author: yuichi_katahira
  */
-public class StatusDialog extends Dialog implements AdapterView.OnItemClickListener {
-    private Activity activity;
+public class StatusDialog extends MenuDialog {
     private Status status;
-    private static final int REPLY = 0;
-    private TweetManager tweetManager;
-    private String[] menuItems;
 
     public StatusDialog(Activity activity, Status status) {
         super(activity);
-        this.activity = activity;
         this.status = status;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.status_dialog);
-
-        SharedManager sharedManager = new SharedManager(activity.getSharedPreferences(Const.PREFERENCE_NAME,
-                activity.MODE_PRIVATE));
-        tweetManager = new TweetManager(sharedManager);
-        ListView menuList = (ListView) findViewById(R.id.status_dialog_list);
-        menuItems = new String[]{activity.getString(R.string.reply), activity.getString(R.string.retweet)};
-        menuList.setAdapter(new ArrayAdapter<String>(activity.getApplicationContext(),
-                android.R.layout.simple_list_item_1, menuItems));
-        menuList.setOnItemClickListener(this);
     }
 
     @Override
