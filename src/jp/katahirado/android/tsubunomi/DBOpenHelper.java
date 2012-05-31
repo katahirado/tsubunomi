344d7562daa0af803f112fc4482f034a3a321bcd
katahirado <yuichi@katahirado.jp>
2012-05-31 14:39:56 +0900

    DBHelperクラスを用意

---
+++ src/jp/katahirado/android/tsubunomi/DBOpenHelper.java
@@ -0,0 +1,29 @@
+package jp.katahirado.android.tsubunomi;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: yuichi_katahira
+ */
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+    private static final String CREATE_SEARCH_WORDS_TABLE = "create table search_words (" +
+            " _id integer primary key autoincrement, word text not null)";
+
+    public DBOpenHelper(Context context) {
+        super(context, "tsubunomi.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(CREATE_SEARCH_WORDS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int position, int position1) {
+    }
+}
+
