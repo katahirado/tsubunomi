0e8e9f85917a4b0f89d5e9465c29c08af25241b1
katahirado <yuichi@katahirado.jp>
2012-05-31 23:17:27 +0900

    ユーザー一覧の親クラスをListActivityからActivityに変更

--- src/jp/katahirado/android/tsubunomi/activity/UsersActivity.java
+++ src/jp/katahirado/android/tsubunomi/activity/UsersActivity.java
@@ -1,9 +1,10 @@
 package jp.katahirado.android.tsubunomi.activity;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import jp.katahirado.android.tsubunomi.Const;
@@ -17,26 +18,27 @@ import java.util.ArrayList;
  * Created with IntelliJ IDEA.
  * Author: yuichi_katahira
  */
-public class UsersActivity extends ListActivity {
+public class UsersActivity extends Activity implements AdapterView.OnItemClickListener {
     private ArrayAdapter<String> adapter;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.users);
-        setTitle(getString(R.string.app_name)+" : 表示ユーザー一覧");
+        setTitle(getString(R.string.app_name) + " : 表示ユーザー一覧");
+        ListView listView = (ListView) findViewById(R.id.users_list);
 
         SharedManager sharedManager = new SharedManager(getSharedPreferences(Const.PREFERENCE_NAME, MODE_PRIVATE));
         ArrayList<String> screenNames = sharedManager.getScreenNames();
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, screenNames);
         adapter.sort(new LowerCaseComparator());
-        setListAdapter(adapter);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
     }
 
     @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Intent intent = new Intent(this,UserTimelineActivity.class);
-        intent.putExtra(Const.SCREEN_NAME,adapter.getItem(position));
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        Intent intent = new Intent(this, UserTimelineActivity.class);
+        intent.putExtra(Const.SCREEN_NAME, adapter.getItem(position));
         startActivity(intent);
     }
 }
