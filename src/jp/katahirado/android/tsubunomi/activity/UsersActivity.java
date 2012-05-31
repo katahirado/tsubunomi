092213ba6cf07bf91facb3dc620bcceead1c94aa
katahirado <yuichi@katahirado.jp>
2012-05-31 20:03:19 +0900

    タイムライン表示済みユーザー一覧の表示まで実装。resourceは作りこまないので削除した

--- src/jp/katahirado/android/tsubunomi/activity/UsersActivity.java
+++ src/jp/katahirado/android/tsubunomi/activity/UsersActivity.java
@@ -1,14 +1,33 @@
 package jp.katahirado.android.tsubunomi.activity;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import jp.katahirado.android.tsubunomi.Const;
+import jp.katahirado.android.tsubunomi.R;
+import jp.katahirado.android.tsubunomi.SharedManager;
+
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
  * Author: yuichi_katahira
  */
-public class UsersActivity extends Activity {
+public class UsersActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedManager sharedManager = new SharedManager(getSharedPreferences(Const.PREFERENCE_NAME, MODE_PRIVATE));
+        ArrayList<String> screenNames = sharedManager.getScreenNames();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, screenNames);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String s = (String) l.getAdapter().getItem(position);
     }
 }
