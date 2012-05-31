ff7d48c29f6441d4730d20e07952ab540b5d2cfe
katahirado <yuichi@katahirado.jp>
2012-05-31 19:35:33 +0900

    タイムラインを表示したことのあるユーザーの一覧画面を用意

---
+++ src/jp/katahirado/android/tsubunomi/activity/UsersActivity.java
@@ -0,0 +1,14 @@
+package jp.katahirado.android.tsubunomi.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: yuichi_katahira
+ */
+public class UsersActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
